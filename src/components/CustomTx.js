ad5807969f17c6863ae65edc6d72b4f81f02c1de
fitnit <nat.coisne@gmail.com>
2023-01-15 21:01:54 +0100

    custom tx dynamic forms prepared

--- src/components/CustomTx.js
+++ src/components/CustomTx.js
@@ -5,50 +5,46 @@ import QRCode from "react-qr-code"
 
 function CustomTx({ myPrivateKey, isInitialized, chain, api, blockExplorer }) {
 
-    const [recipient, setRecipient] = useState('')
-    const [value, setValue] = useState('')
-    const [gasPrice, setGasPrice] = useState('')
-    const [gasLimit, setGasLimit] = useState('')
-    const [nonce, setNonce] = useState('')
     const [text, setText] = useState('')
     const [visible, setVisible] = useState(false)
 
-    const handleRecipientChange = event => {
-        setRecipient(event.target.value)
-    }
-    const handleValueChange = event => { 
-        setValue(event.target.value)
-    }
-    const handleGasPriceChange = event => {
-        setGasPrice(event.target.value)
-    }
-    const handleGasLimitChange = event => {
-        setGasLimit(event.target.value)
-    }
-    const handleNonceChange = event => {
-        setNonce(event.target.value)
+    const [inputFields, setInputFields] = useState([{ 
+        recipient: '', 
+        value: '',
+        gasPrice: '',
+        gasLimit: '',
+        nonce: ''
+        }
+    ])
+
+    const handleFormChange = (index, event) => {
+        let data = [...inputFields];
+        data[index][event.target.name] = event.target.value;
+        setInputFields(data);
     }
 
     const handleSubmit = event => {
         event.preventDefault();
 
         // Check input format
-        if (!ethers.utils.isAddress(recipient) || !(parseFloat(value) > 0)) {
+        if (!ethers.utils.isAddress(inputFields[0].recipient) || !(parseFloat(inputFields[0].value) > 0)) {
             alert('Invalid input format.')
         } else {
             const formData = {
-                recipient, 
-                value, 
-                gasPrice,
-                gasLimit,
-                nonce
+                recipient: inputFields[0].recipient,
+                value: inputFields[0].value,
+                gasPrice: inputFields[0].gasPrice,
+                gasLimit: inputFields[0].gasLimit,
+                nonce: inputFields[0].nonce
             }
             processSubmittedDataFunction(formData);
-            setRecipient('')
-            setValue('')
-            setGasPrice('')
-            setGasLimit('')
-            setNonce('')  
+            setInputFields([{
+                recipient: '',
+                value: '',
+                gasPrice: '',
+                gasLimit: '',
+                nonce: ''
+            }])
         }
     }
     
@@ -64,7 +60,7 @@ function CustomTx({ myPrivateKey, isInitialized, chain, api, blockExplorer }) {
             to: formData.recipient,
             value: ethers.utils.parseEther(formData.value),
             gasPrice: formData.gasPrice*1e9,
-            gasLimit: formData.gasLimit,
+            gasLimit: parseInt(formData.gasLimit),
             nonce: parseInt(formData.nonce)
         }
         
@@ -81,13 +77,6 @@ function CustomTx({ myPrivateKey, isInitialized, chain, api, blockExplorer }) {
         text.length > 0 ? setVisible(true) : setVisible(false)
     }, [text]);
 
-    // create a sizable form for the data of the tx (the user should be able to add as many fields as he wants)
-    // function name
-    // function param1
-    // function param2 
-    // ...
-    // function paramN
-
     return isInitialized ? (
     <div>
         <div className="evm-custom-tx">
@@ -95,55 +84,63 @@ function CustomTx({ myPrivateKey, isInitialized, chain, api, blockExplorer }) {
                 <p>Use this to create custom transactions.</p>
                 <p>Address and uint types are supported</p>
                 <p>WARNING: no data verification is processed, check the correctness of what is typed</p>
-                </div> 
-            <form className="evm-columns" onSubmit={handleSubmit}>
-                <label>
-                    <input 
-                    type="text" 
-                    name="recipient"
-                    value={recipient}
-                    placeholder="Recipient (To)"
-                    onChange={handleRecipientChange}
-                    />
-                </label> <br></br>
-                <label>
-                    <input 
-                    type="text" 
-                    name="value"
-                    value={value}
-                    placeholder="Value of the tx"
-                    onChange={handleValueChange}        
-                    />
-                </label> <br></br>
-                <label>
-                    <input 
-                    type="number" 
-                    name="gasPrice"
-                    value={gasPrice}
-                    placeholder="Gas Price (GWei)"
-                    onChange={handleGasPriceChange}
-                    />
-                </label> <br></br>
-                <label>
-                    <input 
-                    type="number" 
-                    name="gasLimit"
-                    value={gasLimit}
-                    placeholder="Gas Limit"
-                    onChange={handleGasLimitChange}
-                    />
-                </label> <br></br>
-                <label>
-                    <input 
-                    type="number" 
-                    name="nonce"
-                    value={nonce}
-                    placeholder="Nonce"
-                    onChange={handleNonceChange}
-                    />
-                </label> <br></br>
-                <input type="submit" value="Submit and sign" /> 
-            </form>
+            </div> 
+            <div className="evm-columns">
+                <form onSubmit={handleSubmit}>
+                    {inputFields.map((input, index) => {
+                        return (
+                        <div key={index}>
+                            <label>
+                                <input 
+                                type="text" 
+                                name="recipient"
+                                value={input.recipient}
+                                placeholder="Recipient (To)"
+                                onChange={event => handleFormChange(index, event)}
+                                />
+                            </label> <br></br>
+                            <label>
+                                <input 
+                                type="text" 
+                                name="value"
+                                value={input.value}
+                                placeholder="Value of the tx"
+                                onChange={event => handleFormChange(index, event)}        
+                                />
+                            </label> <br></br>
+                            <label>
+                                <input 
+                                type="number" 
+                                name="gasPrice"
+                                value={input.gasPrice}
+                                placeholder="Gas Price (GWei)"
+                                onChange={event => handleFormChange(index, event)}
+                                />
+                            </label> <br></br>
+                            <label>
+                                <input 
+                                type="number" 
+                                name="gasLimit"
+                                value={input.gasLimit}
+                                placeholder="Gas Limit"
+                                onChange={event => handleFormChange(index, event)}
+                                />
+                            </label> <br></br>
+                            <label>
+                                <input 
+                                type="number" 
+                                name="nonce"
+                                value={input.nonce}
+                                placeholder="Nonce"
+                                onChange={event => handleFormChange(index, event)}
+                                />
+                            </label> <br></br>
+                            <input type="submit" value="Submit and sign" /> 
+                        </div>
+                        )
+                    })}
+                </form>
+            </div>
         </div>
         {visible ? 
             <div className="center" >
